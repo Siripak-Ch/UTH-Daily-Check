84eede9cb97296111fd15b5e86504abef5ac7c39
Siripak-Ch <siripak.chat@gmail.com>
2026-06-22 12:19:21 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 /*
-UTH Daily Check Config - FINAL V5.30 SHEET ONLY CONFIG
+UTH Daily Check Config - FINAL V5.31 SHEET ONLY CONFIG
 Frontend uses only this current Apps Script Web App URL.
 No old Apps Script URL is read from localStorage.
 */
 window.DAILY_CHECK_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzpDgkLpMf6dlJp9jzOerqr8giu0d2n4qgaWRtOD4mYEoWXJ8zIKSUUGpAHt8Vr94EhIw/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzkSFyz_Q3mmTpx05aNQs_Bn0M9jX_PdvD1xY3IZ2g_FuPoYiiK5zsX5yYpn0PwL30FKQ/exec",
   SPREADSHEET_ID: "1FZGKlzvMmk9bfB0Et-wQKctf7B8V0gBI0VbN2i2Umw0",
   SHEET_LINK: "https://docs.google.com/spreadsheets/d/1FZGKlzvMmk9bfB0Et-wQKctf7B8V0gBI0VbN2i2Umw0/edit?gid=1719423303#gid=1719423303",
   HOSPITAL_NAME: "โรงพยาบาลอุทัยธานี",
